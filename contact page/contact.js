0ddc045be9d237848cfb024f3e5cf604b2a625ef
sunny singh <sunny24singh@hotmail.com>
2024-10-03 18:43:48 +0530

    git commit -m "js form validation panding"

--- contact page/contact.js
+++ contact page/contact.js
@@ -43,19 +43,90 @@ drop_close.addEventListener("click" , () => {
 
 
 
-let amimaion = document.querySelector("#btn_data");
 
-let i = document.querySelector("#icon_btn");
 
-amimaion.addEventListener("mouseover" , () => {
-  
-     i.animate([
-        // key frames
-        { from: 'margin-left: -100px;' },
-        { to: 'margin-left: 0px;' }
-      ], {
-        // sync options
-        duration: 1000,
-        iterations: ease-out
-      })
+
+
+// form validation
+let input_name1 = document.querySelector("#text")
+let input_email1 = document.querySelector("#email")
+let input_number1 = document.querySelector("#number")
+let input_add1 = document.querySelector("#add")
+let input_sms1 = document.querySelector("#textareas")
+
+
+let forms = document.querySelector("#forms");
+
+forms.addEventListener("submit" , (event) => {
+     event.preventDefault();
+    let input_name = document.querySelector("#text").value;
+    let input_email = document.querySelector("#email").value;
+    let input_number = document.querySelector("#number").value;
+    let input_add = document.querySelector("#add").value;
+    let input_sms = document.querySelector("#textareas").value;
+
+    if (input_name === "" && input_email === "" && input_add === "" && input_number === "" && input_sms === "") {
+        input_name1.style.borderBottom = "1px solid red"
+        input_email1.style.borderBottom = "1px solid red"
+        input_number1.style.borderBottom = "1px solid red"
+        input_add1.style.borderBottom = "1px solid red"
+        input_sms1.style.borderBottom = "1px solid red"
+        
+    } else {
+
+    }
+
+
+    // name validation
+
+    let re = /^[A-Za-z]+$/;
+
+    if (re.test(input_name)) {
+        console.log(0);
+        
+    } else {
+        console.log(1);
+        
+    }
+
+
+    
+
+    // for (let index = 0; index < input_name.length; index++) {
+    //     const element = input_name[index];
+    //     if (isNaN(element)) {
+    //         input_name1.style.borderBottom = ""
+    //     } else {
+            
+    //         input_name1.style.borderBottom = "1px solid red"
+    //     }
+        
+        
+    // }
+
+
+    // email validation
+
+
+    if (isNaN(input_email1) && input_email.includes("@") &&  input_email.includes('.')) {
+        console.log(0);
+    } else {
+        console.log(1);
+    }
+
+
+
+    // number validation
+
+    if (Number(input_number) && input_number.length > 9 && input_number.length < 11) {
+        console.log(0);
+        
+    } else {
+        console.log(1);
+        
+    }
+
+
+    
+    
 })
